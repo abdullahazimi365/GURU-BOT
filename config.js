747b0d3fd17f5a9f5cfddbe2ec27422f2cbe22fb
abdullahazimi365 <152764807+abdullahazimi365@users.noreply.github.com>
2023-12-15 11:08:36 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['923207958720', 'Ashu', true],
+  ['923148265551', 'Ash', true], 
   [''] 
 ] //Number of owners
 
